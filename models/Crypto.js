e0e05f1706475caefd98e718f4a17d69211249fa
D-codess-itt <ddigaf1na@gmail.com>
2026-05-05 00:44:58 +0000

    database-backend fix

--- models/Crypto.js
+++ models/Crypto.js
@@ -4,8 +4,9 @@ const cryptoSchema = new mongoose.Schema({
   name: { type: String, required: true },
   symbol: { type: String, required: true },
   price: { type: Number, required: true },
-  image: { type: String, required: true },
+  image: { type: String, required: false },
   change24h: { type: Number, required: true }, // e.g., 2.5 or -1.4
+  marketCap: { type: String, required: true },
 }, { timestamps: true });
 
 export default mongoose.model('Crypto', cryptoSchema);
